034cc13562aa73ead386555a14ba08cdc75c99fc
Jander Guimarães Freire <janderrv26@gmail.com>
2023-09-01 18:25:34 -0400

    fix: fix npm start

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ const telegramService = new TelegramService(config.TELEGRAM_TOKEN);
 const rabbitMQService = new RabbitMQService();
 
 rabbitMQService.connect(config.RABBITMQ_URL).then(() => {
+	console.log('RabbitMQ connected');
 	rabbitMQService.consume(config.TELEGRAM_QUEUE, (message) => {
+		console.log('Message received');
 		const image = Buffer.from(message.image.data);
 		telegramService.sendPhoto(config.CHAT_ID, image);
 	});
